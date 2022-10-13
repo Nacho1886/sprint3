c642dd877597062c2ef0a8e5c8f98a3943b02297
Nacho1886 <francert@hotmail.com>
2022-10-13 13:00:41 +0200

    maquetando sprint 3

--- js/checkout.js
+++ js/checkout.js
@@ -2,47 +2,46 @@
 // Exercise 6
 function validate() {
 	var error = 0;
+	const forms = document.querySelectorAll('.needs-validation')
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
-
+	// form.classList.add(is-valid, is-invalid)
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
 	var errorEmail = document.getElementById("errorEmail");
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == "" || burro){
+	if (fName.value == "" || burro) {
 		error++;
-		errorName.is
-	}
+		if (fEmail.value == "") {
+			error++;
+		}
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
+		if (error > 0) {
 
-	}else{
-		alert("OK");
-		// window.addEventListener.
-	}
+		} else {
+			alert("OK");
+			// window.addEventListener.
+		}
 
+	}
 }
 
-// (() => {
-// 	'use strict'
+	(() => {
+		'use strict'
 
-// 	// Fetch all the forms we want to apply custom Bootstrap validation styles to
-// 	const forms = document.querySelectorAll('.needs-validation')
-// 	// Loop over them and prevent submission
-// 	Array.from(forms).forEach(form => {
-// 		form.addEventListener('submit', event => {
-// 			console.log(forms);
-// 			if (!form.checkValidity()) {
-// 				event.preventDefault()
-// 				event.stopPropagation()
-// 			}
+		/* Fetch all the forms we want to apply custom Bootstrap validation styles to */
+		const forms = document.querySelectorAll('.needs-validation')
+		/* Loop over them and prevent submission */
+		Array.from(forms).forEach(form => {
+			form.addEventListener('submit', event => {
+				console.log(forms);
+				if (!form.checkValidity()) {
+					event.preventDefault()
+					event.stopPropagation()
+				}
 
-// 			form.classList.add('was-validated')
-// 		}, false)
-// 	})
-// })()
+				form.classList.add('was-validated')
+			}, false)
+		})
+	})()
