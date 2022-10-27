99f038120f7c7024a1836333f1586378e6a86bdf
Nacho1886 <francert@hotmail.com>
2022-10-27 18:25:35 +0200

    last change refact

--- js/checkout.js
+++ js/checkout.js
@@ -3,12 +3,12 @@
 const bottonAppear = document.querySelector('#btn')
 const forms = document.querySelectorAll('.form-control')
 const validator = [
-{name: 'fName', expression: /^[A-z]{3,20}$/, counter: 0},
-{name: 'fLastN', expression: /^[A-z]{3,15}$/, counter: 0},
-{name: 'fEmail', expression: /^[^]+@[^]+\.[A-z]{2,3}$/, counter: 0},
-{name: 'fPassword', expression: /^[A-z0-9]{4,8}$/, counter: 0},
-{name: 'fAddress', expression: /^[A-Za-z0-9'\.\-\s\,]{3,80}$/, counter: 0},
-{name: 'fPhone', expression: /^[0-9]{9}$/, counter: 0},
+{name: 'fName', expression: /^[A-z]{3,20}$/, isCorrect: false},
+{name: 'fLastN', expression: /^[A-z]{3,15}$/, isCorrect: false},
+{name: 'fEmail', expression: /^[^]+@[^]+\.[A-z]{2,3}$/, isCorrect: false},
+{name: 'fPassword', expression: /^[A-z0-9]{4,8}$/, isCorrect: false},
+{name: 'fAddress', expression: /^[A-Za-z0-9'\.\-\s\,]{3,80}$/, isCorrect: false},
+{name: 'fPhone', expression: /^[0-9]{9}$/, isCorrect: false},
 ]
 
 forms.forEach((form) => {
@@ -19,8 +19,8 @@ forms.forEach((form) => {
 		if (inputIsValid) {
 			form.classList.remove("is-invalid")
 			form.classList.add("is-valid")
-			locateValidator.counter = 1
-			const invalidForm = validator.find((e) => e.counter === 0)
+			locateValidator.isCorrect = true
+			const invalidForm = validator.find((e) => e.isCorrect === false)
 			if (!invalidForm) {
 				bottonAppear.removeAttribute('disabled')
 			}
@@ -28,7 +28,7 @@ forms.forEach((form) => {
 		if (!inputIsValid) {
 			form.classList.remove("is-valid")
 			form.classList.add("is-invalid")
-			locateValidator.counter = 0
+			locateValidator.isCorrect = false
 			bottonAppear.setAttribute('disabled', '')
 		}
 	})
